0dbd2636191b764a799292683ecbb224de464195
Börge Meyer <boergermeisterr@icloud.com>
2023-04-19 12:01:45 +0200

    Home & Lektion, Grafiken

--- src/views/LektionView.vue
+++ src/views/LektionView.vue
@@ -1,68 +1,64 @@
 <template>
-    <head>
-        <meta name="title" content="Lernplattform für Python">
-        <meta name="description" content="Eine Lernplattform, um die Programmiersprache Python zu erlernen.">
-        <meta name="keywords" content="Lernen, Plattform, Lernplattform, python, deep learning, künstliche neuronale netze, machine learning, ki, chatgpt, programmieren, program">
-        <meta name="robots" content="index, nofollow">
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-16">
-        <meta name="language" content="English">
-        <meta name="revisit-after" content="7 days">
-        <meta name="author" content="Technikgruppe der HSBA - A20-BI2 & A20-MMC2">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-        <title>Learning Plattform</title>
-    </head>
-    <body>
-        <!-- Navigation -->
-        <!-- Icons by Google: https://fonts.google.com/icons -->
-        <div class="container-fluid p-0 fixed-top">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <img style="height:52px" src="public/1200px-HSBA_Hamburg_School_of_Business_Administration_Logo_2018_nur_Wappen.svg.png" class="img-thumbnail px-1" alt="...">
-                <a class="navbar-brand fw-bolder px-2" href="index.html">Digital Labour</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="lektion1.html">
-                        <div class="container text-center">
-                            <div class="row align-items-start">
-                                <div class="col p-0">
-                                    <span class="material-symbols-outlined">
-                                        school
-                                    </span>
-                                </div>
-                                <div class="col fw-bold">
-                                    Lektionen                           
-                                </div>
-                            </div>
-                        </div>    
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <div class="container text-center">
-                            <div class="row align-items-start">
-                                <div class="col p-0">
-                                    <span class="material-symbols-outlined">
-                                    shopping_cart
-                                    </span>
-                                </div>
-                                <div class="col fw-bold">
-                                    Preisliste                           
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </li>
-                </ul>
-                <a class="nav-link d-flex" href="login.html">
+<head>
+    <meta name="title" content="Lernplattform für Python">
+    <meta name="description" content="Eine Lernplattform, um die Programmiersprache Python zu erlernen.">
+    <meta name="keywords" content="Lernen, Plattform, Lernplattform, python, deep learning, künstliche neuronale netze, machine learning, ki, chatgpt, programmieren, program">
+    <meta name="robots" content="index, nofollow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-16">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="7 days">
+    <meta name="author" content="Technikgruppe der HSBA - A20-BI2 & A20-MMC2">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Learning Plattform</title>
+</head>
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary z-1 position-absolute container-fluid">
+        <div class="container-fluid">
+            <img style="height:52px" src="img/HSBA_Logo.svg" class="px-1" alt="...">
+            <form class="d-flex my-auto mx-4 rounded" role="search">
+                <input class="form-control me-2 rounded-pill fw-bold" type="search" placeholder="Suchen nach..." aria-label="Search">
+            </form>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li>
+                  <a class="nav-link" href="login.html">
                     <div class="container text-center">
                         <div class="row align-items-start">
                             <div class="col p-0">
-                            <span class="material-symbols-outlined">
+                                <span class="material-symbols-outlined">
+                                    home
+                                </span>
+                            </div>
+                            <div class="col fw-bold">
+                                Home                           
+                            </div>
+                        </div>
+                    </div>
+                  </a>
+              </li>
+              <li>
+                <a class="nav-link" href="lektion1.html">
+                    <div class="container text-center">
+                        <div class="row align-items-start">
+                            <div class="col p-0">
+                                <span class="material-symbols-outlined">
+                                    school
+                                </span>
+                            </div>
+                            <div class="col fw-bold">
+                                Kurse                           
+                            </div>
+                        </div>
+                    </div>
+                  </a>
+              </li>
+              <li>
+                <a class="nav-link" href="login.html">
+                    <div class="container text-center">
+                        <div class="row align-items-start">
+                            <div class="col p-0">
+                              <span class="material-symbols-outlined">
                                 login
                                 </span>
                             </div>
@@ -71,117 +67,104 @@
                             </div>
                         </div>
                     </div>
-                </a>
-                <a class="nav-link d-flex" href="register.html">
+                  </a>
+              </li>
+              <li>
+                <a class="nav-link" href="register.html">
                     <div class="container text-center">
-                    <div class="row align-items-start">
-                        <div class="col p-0">
+                      <div class="row align-items-start">
+                          <div class="col p-0">
                             <span class="material-symbols-outlined">
-                            input
-                            </span>
-                        </div>
-                        <div class="col fw-bold">
+                              input
+                              </span>
+                          </div>
+                          <div class="col fw-bold">
                             Regstrierung                           
                         </div>
+                      </div>
                     </div>
-                    </div>
-                </a>
-            </div>
-            </div>
-        </nav> 
+                  </a>
+              </li>
+            </ul>
         </div>
+    </nav>
 
-        <div class="container-xl">
-            <div class="row">
-            <div class="col-2 border-end">
-                <ul class="nav flex-column pt-5 mt-5">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Lektion 1</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 2</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 3</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 4</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 5</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 6</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 7</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 8</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 9</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Lektion 10</a>
-                    </li>
-                </ul>
+    <!-- content -->
+    <div class="container-fluid py-5 px-5">
+        <!-- title -->
+        <div class="container-fluid row pt-5">
+            <h1 class="fw-bold fs-1" style="color: #002B5C;">Technikkurs - Lektion 1</h1>
+        </div>
+        <!-- left -->
+        <div class="row">
+            <div class="col-2 py-5">
+                <nav class="nav flex-column">
+                    <a class="nav-link rounded-pill my-2 text-white text-center" style="background-color: #002B5C;" href="#">Lektion 1</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 2</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 3</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 4</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 5</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 6</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 7</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 8</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 9</a>
+                    <a class="nav-link bg-body-tertiary rounded-pill my-2 text-black text-center" href="#">Lektion 10</a>
+                </nav>
             </div>
-            <div class="col">
-                <!-- Lektion 1 - Content -->
-                <!-- Überschrift -->
-                    <p class="fs-2">
-                        Lektion 1
+    
+            <!-- right -->
+            <div class="col py-5">
+                <div class="container-fluid p-4 bg-body-tertiary rounded">
+                    <!-- Text -->
+                    <p class="lh-base fw-light py-4">
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, 
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam 
+                        erat, sed diam voluptua. At vero eos et accusam et justo duo 
+                        dolores et ea rebum. 
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit 
+                        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
+                        eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+                        dolor sit amet.
                     </p>
-                <!-- Beschreibung -->
-                    <p class="fw-light">
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    </p>
-                <!-- Video -->
-                <div class="container-fluid m-2 mb-3">
-                    <div class="d-flex justify-content-center">
-                        <iframe class="object-fit-fill border rounded" width="90%" height="550px" src="https://www.youtube.com/embed/9mmVa6O-hzQ?controls=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <!-- Video -->
+                    <div class="d-flex justify-content-center py-4">
+                        <iframe class="object-fit-fill border rounded" style="width: 100%; height: 650px" src="https://www.youtube.com/embed/9mmVa6O-hzQ?controls=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     </div>
-                </div>
-                <!-- Hinweise bzw. weitere Informationen -->
-                <div class="container-fluid">
-                    <div class="accordion accordion-flush" id="accordionFlushExample">
-                        <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Accordion Item #1
-                            </button>
-                        </h2>
-                        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-                        </div>
-                        </div>
-                        <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                            Accordion Item #2
-                            </button>
-                        </h2>
-                        <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-                        </div>
-                        </div>
-                        <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                            Accordion Item #3
-                            </button>
-                        </h2>
-                        <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                            <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-                        </div>
-                        </div>
+                    <!-- Code Abschnitt -->
+                    <div class="py-4">
+                        <p class="fw-bold">Task:</p>
+                        <p class="lh-base fw-light">
+                            Write a function to get the volume of a sphere with radius 6.
+                        </p>
+                        <form>
+                            <div class="mb-3">
+                                <div class="input-group">
+                                    <span class="input-group-text">Code</span>
+                                    <textarea class="form-control" aria-label="With textarea" style="min-height: 250px" placeholder="math import pi def calc_sphere_volume(r=6): sphere_volume == 4/3*pi*r**2 return sphere_volume"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-11">
+                                    <button type="submit" class="btn btn-primary rounded-pill" style="background-color: #002B5C; border: none">Code prüfen</button>
+                                    <button type="submit" class="btn btn-primary rounded-pill" style="background-color: #002B5C; border: none">Nach Tipp fragen</button>
+                                </div>
+                                <div class="col-1">
+                                    <button type="submit" class="btn btn-primary rounded-pill" style="background-color: #002B5C; border: none">
+                                        <span class="material-symbols-outlined circle">
+                                            smart_toy
+                                        </span>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
-
-            </div>
         </div>
+    </div>
 
-    </body>
+</body>
 </template>
