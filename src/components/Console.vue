8f504ec38c347fc709ecd797d86f2ecd1d104a08
olive <oliver.lorenz@gmx.com>
2023-05-02 11:11:16 +0200

    removed toggle from console

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="h-full">
-        <button class="bg-gray-400 hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border-0 border-green-500 rounded-lg shadow mr-5">Output</button>
-        <button class="bg-gray-400 hover:bg-gray-200 text-gray-800 font-semibold py-2 px-4 border-0 border-green-500 rounded-lg shadow">Help</button>
+    <div class="h-full mt-8">
         <ScrollPanel class="h-full">
             <section>
                 <p style="background-color: #282c34; white-space: pre-line;">{{ current == "consolelog" ? consolelog : help}}</p>
