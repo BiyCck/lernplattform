5d6a4bd2f6c7de1a02831b0a16344182b2948f32
olive <oliver.lorenz@gmx.com>
2023-05-02 11:05:11 +0200

    home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,7 @@
 <template>
-<head>
-    <meta name="title" content="Lernplattform für Python">
-    <meta name="description" content="Eine Lernplattform, um die Programmiersprache Python zu erlernen.">
-    <meta name="keywords" content="Lernen, Plattform, Lernplattform, python, deep learning, künstliche neuronale netze, machine learning, ki, chatgpt, programmieren, program">
-    <meta name="robots" content="index, nofollow">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-16">
-    <meta name="language" content="English">
-    <meta name="revisit-after" content="7 days">
-    <meta name="author" content="Technikgruppe der HSBA - A20-BI2 & A20-MMC2">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <title>Learning Plattform</title>
-</head>
-<body>
-    <navigation-bar />
-     <!-- Header -->
+
+<main class="bg-gray-800 text-white pt-40 h-screen flex justify-center space-x-10">
+    
     <div id="header">
         <div class="position-absolute" style="top: 25%; left: 25px;">
             <p class="text-start fw-bold" style="color: 002B5C; font-size: 75px;">
@@ -25,23 +12,9 @@
             <a type="button" class="btn btn-primary fs-3 fw-bold rounded-pill px-5" style="background-color: #002B5C; border: none;">KLICKEN</a>
         </div>
     </div>
-</body>
- <style> 
-         html, body, #app {height: 100%}
-        #header{
-          height: 100%; 
-          background-image: url('../../public/header.svg'); 
-          background-repeat: no-repeat; 
-          background-position: 50%; 
-          background-size: cover;
-        }
-        #categories{
-          height: 100%; 
-          background-image: url('../../public/categories.svg'); 
-          background-repeat: no-repeat; 
-          background-position: 50%; 
-          background-size: cover;
-        }
-</style>
-<script>
+  </main>
+</template>
+
+<script setup>
 import { onBeforeMount } from "vue";
+</script>
