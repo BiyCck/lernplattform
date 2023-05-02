edb89e2fb047ccb721a5569ca331cf4bc9de8036
olive <oliver.lorenz@gmx.com>
2023-05-02 11:29:03 +0200

    fixed list error

--- src/components/RenderElement.vue
+++ src/components/RenderElement.vue
@@ -1,21 +1,21 @@
 <template>
 
-        <code v-if="props.type === 'code'" style=" white-space: pre-line;">{{ props.value }}</code>
+        <code v-if="props.type == 'code'" style=" white-space: pre-line;">{{ props.value }}</code>
     
   
         
    
    
-        <p v-if="props.type === 'text'" class="text-justify">{{ props.value }}</p>
+        <p v-if="props.type == 'text'" class="text-justify">{{ props.value }}</p>
 
 
-        <img v-if="props.type === 'image'" :src="'@/assets/' + props.value + '.png'" />
+        <img v-if="props.type == 'image'" :src="'@/assets/' + props.value + '.png'" />
 
 
-        <h2 v-if="props.type === 'heading'" class="text-2xl font-bold"> {{ props.value }}</h2>
+        <h2 v-if="props.type == 'heading'" class="text-2xl font-bold"> {{ props.value }}</h2>
 
 
-        <ul v-if="props.type === 'list'" class="list-disc list-inside">
+        <ul v-if="Array.isArray(props.value)" class="list-disc list-inside">
             <li v-for="item in props.value">{{ item }}</li>
         </ul>
         <br>
