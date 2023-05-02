ece519cc008476fec4ededdfabc654891943d5da
olive <oliver.lorenz@gmx.com>
2023-05-02 11:47:07 +0200

    fixed render images

--- src/components/RenderElement.vue
+++ src/components/RenderElement.vue
@@ -9,7 +9,7 @@
         <p v-if="props.type == 'text'" class="text-justify">{{ props.value }}</p>
 
 
-        <img v-if="props.type == 'image'" :src="'@/assets/' + props.value + '.png'" />
+        <img v-if="props.type == 'image'" :id="props.value"/>
 
 
         <h2 v-if="props.type == 'heading'" class="text-2xl font-bold"> {{ props.value }}</h2>
@@ -24,6 +24,32 @@
 
 <script setup>
 const props = defineProps(["type", "value"]);
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
+if (props.type === "image"){
+    const storage = getStorage();
+getDownloadURL(ref(storage, props.value + '.png'))
+  .then((url) => {
+    // `url` is the download URL for 'images/stars.jpg'
+
+    // This can be downloaded directly:
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = (event) => {
+      const blob = xhr.response;
+    };
+    xhr.open('GET', url);
+    xhr.send();
+
+    // Or inserted into an <img> element
+    const img = document.getElementById(props.value);
+    img.setAttribute('src', url);
+  })
+  .catch((error) => {
+    // Handle any errors
+  });
+}
+
 
 
 </script>
